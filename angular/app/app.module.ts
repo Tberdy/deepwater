63623502739db76e0c2c5bf19628518d0fa3b65d
Thomas Berdy <tberdy@hotmail.fr>
2018-01-28 00:12:11 +0100

    - header & footer

--- angular/app/app.module.ts
+++ angular/app/app.module.ts
@@ -5,19 +5,28 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {HttpClientModule} from '@angular/common/http';
 
+import {MatButtonModule} from '@angular/material/button';
+
 import {AppRoutingModule} from './app-routing.module';
+
 import {AppComponent} from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
     declarations: [
         AppComponent,
+        HeaderComponent,
+        FooterComponent,
     ],
     imports: [
         BrowserModule,
         AppRoutingModule,
         BrowserAnimationsModule,
         FlexLayoutModule,
-        HttpClientModule
+        HttpClientModule,
+        MatButtonModule
+        
     ],
     providers: [],
     bootstrap: [AppComponent]
