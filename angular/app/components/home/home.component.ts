d681984249372913a4200c9313b3cde22d68bc68
Kingsun21 <t.grimault21@gmail.com>
2018-03-20 17:26:48 +0100

    footer + slider's banner

--- angular/app/components/home/home.component.ts
+++ angular/app/components/home/home.component.ts
@@ -12,11 +12,13 @@ export class HomeComponent implements OnInit {
   items: Array<any> = [];
   constructor() {
     this.items = [
-      { name: 'assets/images/thumb1.png' },
-      { name: 'assets/images/thumb2.png' },
-      { name: 'assets/images/thumb3.png' }
+      { name: 'assets/images/thumb1.png', text : 'Organisez des séances avec votre groupe !' },
+      { name: 'assets/images/thumb2.png', text : 'Entrainez vous avec vos amis !' },
+      { name: 'assets/images/thumb3.png', text : 'Repoussez vos limites avec OhSaySport !' }
     ]
   }
+
+
 
   ngOnInit() {
   }
