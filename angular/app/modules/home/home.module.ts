3278d4514215eeaaa80e12342b6037362b9a627f
Thomas Berdy <tberdy@hotmail.fr>
2018-03-23 12:23:11 +0100

    - merge

--- angular/app/modules/home/home.module.ts
+++ angular/app/modules/home/home.module.ts
@@ -2,8 +2,8 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {MatButtonModule, MatInputModule, MatIconModule, MatSnackBarModule, MatStepperModule} from '@angular/material';
-import { Ng2CarouselamosModule } from 'ng2-carouselamos';
-import { HomeRoutingModule } from './home-routing.module';
+import {Ng2CarouselamosModule} from 'ng2-carouselamos';
+import {HomeRoutingModule} from './home-routing.module';
 import {HomeComponent} from '../../components/home/home.component';
 
 @NgModule({
