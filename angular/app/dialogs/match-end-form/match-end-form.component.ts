3c45b31fa4dde3544a988e310b91d6c0ea6cdea8
Alain BERRIER <alain.berrier@outlook.com>
2018-03-25 15:20:36 +0200

    changes

--- angular/app/dialogs/match-end-form/match-end-form.component.ts
+++ angular/app/dialogs/match-end-form/match-end-form.component.ts
@@ -1,7 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, Inject} from '@angular/core';
 import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
 
-import {Contest} from '../../models/contest';
 import {Member} from '../../models/member';
 @Component({
     selector: 'app-match-end-form',
@@ -10,7 +9,20 @@ import {Member} from '../../models/member';
 })
 export class MatchEndFormDialog implements OnInit {
 
-    constructor(public dialogRef: MatDialogRef<MatchEndFormDialog>) {}
+    opponent: Member;
+    user: Member;
+    users: Member[];
+    winner : Member;
+    looser : Member;
+    draw: boolean;
+
+    constructor(public dialogRef: MatDialogRef<MatchEndFormDialog>,
+        @Inject(MAT_DIALOG_DATA) public data: any) {
+        this.opponent = data.opponent;
+        this.user = data.user;
+        this.users = [this.user, this.opponent];
+        this.draw = false;
+    }
 
     ngOnInit() {
     }
